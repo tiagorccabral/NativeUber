66cec9daefe4ff49652c211017973b52936fa5f1
Tiago Cabral <tiagorc.cabral@gmail.com>
2019-08-08 19:21:19 -0300

    Adiciona react native maps

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import Map from './components/Map';
 import { View } from 'react-native';
 
 // import { Container } from './styles';
 
 export default class App extends Component {
   render() {
-    return <View />;
+    return (
+      <View style={{ flex: 1 }}>
+        <Map />
+      </View>
+    );
   }
 }
